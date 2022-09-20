2fc36003054f09aa86ecf514d37a11d0c2d05f6e
MS <108895850+Saifudeebot@users.noreply.github.com>
2022-09-20 15:25:36 +0530

    Update __init__.py

--- Ms_Robot/__init__.py
+++ Ms_Robot/__init__.py
@@ -1,1 +1,41 @@
+from .Utils import (
+   get_filter_results,
+   get_file_details,
+   is_subscribed,
+   get_poster,
+   Media
+)
+from .Channel import (
+   RATING,
+   GENRES
+)
 
+HELP = """
+Basic Commads
+
+/start :- check am Alive
+
+Bot Owner Only
+/broadcast Replay Any Message or Media
+/stats user Status
+/ban_user  click ban_user more info
+/unban_user click unban_user more info
+/banned_users Banned User Details
+/total How Many Files Added In Database
+/logger  Get Logs
+/delete Delete File From Database
+
+"""
+
+ABOUT = """
+➥ Name :  MS Auto Filter V3
+    
+➥ Creator : [ms_alone1](
+https://youtube.com/channel/UC_TA8iDJCF9FvRN4YGyDNiA?utm_source=EKLEiJECCKjOmKnC5IiRIQ)
+
+➥ Language : Python3
+
+➥ Library : Pyrogram Asyncio 1.13.0
+
+➥ Tutorial : [Click Me](https://youtube.com/watch?v=QSsYfSmWnRE&feature=share&utm_source=EKLEiJECCKjOmKnC5IiRIQ)
+"""
